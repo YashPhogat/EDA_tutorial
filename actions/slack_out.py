888f36b903e7d355f3f81905c358de35f04f7152
root <yashnitd@gmail.com>
2020-06-02 03:17:35 +0000

    python file change

--- actions/slack_out.py
+++ actions/slack_out.py
@@ -4,7 +4,7 @@ import requests
 class message_out(Action):
 	def run (self, message):
 		body = { "text": message}
-		webhook_url = "https://hooks.slack.com/services/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"
+		webhook_url = "https://hooks.slack.com/services/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"
 		response = requests.post(url=webhook_url, json=body)
 		response.raise_for_status()
 		
