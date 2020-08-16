d28b61f0e65e9f09d3c73bdf35836b86add1fabe
Yash Phogat <yashnitd@gmail.com>
2020-08-16 12:48:43 -0700

    hide webhook url

--- actions/slack_out.py
+++ actions/slack_out.py
@@ -4,7 +4,7 @@ import requests
 class message_out(Action):
 	def run (self, message):
 		body = { "text": message}
-		webhook_url = "https://hooks.slack.com/services/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"
+		webhook_url = "<slack_incoming_webhook_url>"
 		response = requests.post(url=webhook_url, json=body)
 		response.raise_for_status()
 		
